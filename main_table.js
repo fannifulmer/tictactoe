534bd10deb277587cce298ab103efa33a7ff40b6
Mate Bodor <bodormate@gmail.com>
2017-08-30 22:17:51 +0200

    first tasks

--- main_table.js
+++ main_table.js
@@ -1,8 +1,66 @@
 'use strict';
-const board = document.getElementById("board");
+const board = document.querySelector(".board");
 
 let matrix = [
   ['', '', ''],
   ['', '', ''],
   ['', '', '']
 ]
+
+let player = 'x'
+
+//Practice: Add, one cell to the board and set an X in it.
+function addCell(board){
+
+}
+
+function setCellValue(cell){
+
+}
+
+//-- Main code: Loop through the matrix and draw it’s values
+
+function addHtmlElementToBoard(board){
+
+}
+
+function renderBoard(matrix) {
+
+}
+
+//-- Main code: Setter
+
+function setPlayer(){
+
+}
+
+function setMatrix(position, player){
+
+}
+
+//Test your solution
+// setMatrix([0,1],'O')
+
+//-- Main code: Event-listener
+
+function addEventListener(){
+
+}
+
+//-- Main code: checker
+
+function isLineWin(matrix){
+
+}
+
+function isWon(){
+
+}
+
+function isGameOver(){
+
+}
+
+//-- Main code: Game //Invite the functions.
+
+//
