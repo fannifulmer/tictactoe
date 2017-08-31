f5b3166dc3befed1761b3b0eedd9b0f30c887353
geritol <gergo.tolnai@gmail.com>
2017-08-31 06:52:30 +0200

    switched BOARDWIDTH and BOARDHEIGHT to BOARDSIZEPX, as we will have only sure boards and tiles

--- starter-advanved/main.js
+++ starter-advanved/main.js
@@ -7,20 +7,19 @@ let matrix = [
   ['', '', '']
 ]
 
-const TILESIZE = 10;
+const TILESIZE = 3;
 const TILEBORDER_WIDTH = 2;
 const TOTAL_BORDERLINES = (TILESIZE * 2) - 2;
-const BOARDWIDTH = 600;
-const BOARDHEIGHT = 600;
-const SPACING = BOARDWIDTH / (TOTAL_BORDERLINES/2 + 1);
+const BOARDSIZEPX = 300;
+const SPACING = BOARDSIZEPX / (TOTAL_BORDERLINES/2 + 1);
 
 function initBoard(){
   canvasContext.clearRect(0, 0, canvas.width, canvas.height);
   for(let i = 0; i < TOTAL_BORDERLINES; i++){
     if(i < TOTAL_BORDERLINES/2){
-      canvasContext.fillRect(SPACING * (i + 1),0, TILEBORDER_WIDTH, BOARDHEIGHT);
+      canvasContext.fillRect(SPACING * (i + 1),0, TILEBORDER_WIDTH, BOARDSIZEPX);
     }else{
-      canvasContext.fillRect(0,SPACING * (i - TOTAL_BORDERLINES/2 + 1), BOARDWIDTH, TILEBORDER_WIDTH);
+      canvasContext.fillRect(0,SPACING * (i - TOTAL_BORDERLINES/2 + 1), BOARDSIZEPX, TILEBORDER_WIDTH);
     }
   }
 }
